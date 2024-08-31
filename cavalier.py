841cff9de77f8b0c36e5e0774d9922fd85ba267e
6ce <86327263+6ce@users.noreply.github.com>
2024-08-31 10:51:04 -0700

    Add email & ip searching (for devs)

--- cavalier.py
+++ cavalier.py
@@ -5,18 +5,27 @@ class Cavalier:
     def __init__(self):
         self.api = "https://cavalier.hudsonrock.com/api/json/v2"
 
-    def _get(self, path: str) -> requests.Response:
+    def _get(self, path: str) -> list:
         """Performs a get request to the input API path on https://cavalier.hudsonrock.com"""
         url = self.api + path
-        return requests.get(url)
-
-    def usernameSearch(self, username: str) -> list | str:
-        """Performs a username search on https://cavalier.hudsonrock.com"""
-        path = "/osint-tools/search-by-username?username={}".format(username)
-        response = self._get(path)
-        
+        response = requests.get(url)
         try:
             data = response.json()
             return data["stealers"]
         except:
-            return response.text
+            return []
+
+    def usernameSearch(self, username: str) -> list:
+        """Performs a username search on https://cavalier.hudsonrock.com"""
+        path = "/osint-tools/search-by-username?username={}".format(username)
+        return self._get(path)
+    
+    def emailSearch(self, email: str) -> list:
+        """Performs an email search on https://cavalier.hudsonrock.com"""
+        path = "/osint-tools/search-by-email?email={}".format(email)
+        return self._get(path)
+    
+    def ipSearch(self, ip: str) -> list:
+        """Performs an IP address search on https://cavalier.hudsonrock.com"""
+        path = "/osint-tools/search-by-ip?ip={}".format(ip)
+        return self._get(path)
